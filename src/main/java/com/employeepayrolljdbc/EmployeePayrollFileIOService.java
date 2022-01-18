5198059ed8680fd1364294bbe55f006d5c900af7
Srinath S <srinath@Srinaths-MacBook-Air.local>
2022-01-18 19:25:01 +0530

    Added : Service to retrieve employee payroll from database

---
+++ src/main/java/com/employeepayrolljdbc/EmployeePayrollFileIOService.java
@@ -0,0 +1,15 @@
+package com.employeepayrolljdbc;
+
+import java.util.List;
+
+public class EmployeePayrollFileIOService {
+    public void writeData(List<EmployeePayrollData> employeePayrollList) {
+
+    }
+
+    public void printData() {
+    }
+
+    public void countEntries() {
+    }
+}
---
+++ src/test/java/com/employeepayrolljdbc/EmployeePayrollServiceTest.java
@@ -0,0 +1,35 @@
+package com.employeepayrolljdbc;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static com.employeepayrolljdbc.EmployeePayrollService.IOService.DB_IO;
+import static com.employeepayrolljdbc.EmployeePayrollService.IOService.FILE_IO;
+
+public class EmployeePayrollServiceTest {
+
+    @Test
+    public void given3EmployeesWhenWrittenToFileShouldMatchEmployeeEntries() {
+        EmployeePayrollData[] arraysOfEmployees = {
+                new EmployeePayrollData(1, "Jeff Bezos", 100000.0),
+                new EmployeePayrollData(2, "Bill Gates", 200000.0),
+                new EmployeePayrollData(3, "Elon Mask", 300000.0),
+        };
+        EmployeePayrollService employeePayrollService;
+        employeePayrollService = new EmployeePayrollService(Arrays.asList(arraysOfEmployees));
+        employeePayrollService.writeEmployeePayrollData(FILE_IO);
+        employeePayrollService.printData(FILE_IO);
+        long entries = employeePayrollService.countEntries(FILE_IO);
+        Assertions.assertEquals(3, entries);
+    }
+
+    @Test
+    public void givenEmployeePayrollInDB_WhenRetrieved_ShouldMatchEmployeeCount(){
+        EmployeePayrollService employeePayrollService = new EmployeePayrollService();
+        List<EmployeePayrollData> employeePayrollData = employeePayrollService.readEmployeePayrollData(DB_IO);
+        Assertions.assertEquals(3,employeePayrollData.size());
+    }
+}
